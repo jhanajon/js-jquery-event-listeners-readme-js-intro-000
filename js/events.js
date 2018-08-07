4de80c5d8982bc0232ef39195ec3750cd9982abf
joe fisher <jhanajon@gmail.com>
2018-08-07 01:53:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -28,12 +28,9 @@ function pressIt() {
 }
 
 function submitIt() {
-  $("form").on("submit", function() {
-  if ($( "input:first" ).val === "correct") {
-    alert('your form is going to be submitted now');
-    return;
-    }
-  });
+$("form").submit(function(){
+    alert("your form is going to be submitted now");
+});
 }
 
 
